2ec05d4a560e90d0e2058bcfde30db6ec44f9960
Mehul Pathak <pathakkmehul@gmail.com>
2023-11-06 18:51:07 +0530

    Time: 32 ms (80.95%), Space: 13.4 MB (16.81%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        result = []
+        self.combinationSumRecu(sorted(candidates), result, 0, [], target)
+        return result
+
+    def combinationSumRecu(self, candidates, result, start, intermediate, target):
+        if target == 0:
+            result.append(list(intermediate))
+        while start < len(candidates) and candidates[start] <= target:
+            intermediate.append(candidates[start])
+            self.combinationSumRecu(candidates, result, start, intermediate, target - candidates[start])
+            intermediate.pop()
+            start += 1
