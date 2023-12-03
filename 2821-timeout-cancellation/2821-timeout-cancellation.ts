2b560b72dc100999450036dc59942dc3e51f2273
Mehul Pathak <pathakkmehul@gmail.com>
2023-12-03 21:31:29 +0530

    Time: 73 ms (22.52%), Space: 42.9 MB (65.04%) - LeetHub

---
+++ 2821-timeout-cancellation/2821-timeout-cancellation.ts
@@ -0,0 +1,33 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+    const id = setTimeout(() => fn(...args), t);
+    return () => { clearTimeout(id); }
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const start = performance.now() 
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)})
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelT)
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *
+ *  setTimeout(() => {
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
