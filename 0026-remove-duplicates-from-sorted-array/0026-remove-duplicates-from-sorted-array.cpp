8458bda8969b5a97236affce84dfece873cc7a05
Mehul Pathak <pathakkmehul@gmail.com>
2024-01-21 16:04:11 +0530

    Time: 7 ms (79.62%), Space: 21 MB (5.03%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(NULL);
-        cout.tie(NULL);
         int c=0;
         for(int i=1;i<nums.size();i++){
             if(nums[c]!=nums[i]){
