9b2073e5ebdf5bb6b4e5649e3ab71c7a51f01e2a
Mehul Pathak <pathakkmehul@gmail.com>
2024-01-22 16:01:56 +0530

    Time: 167 ms (78.29%), Space: 118.7 MB (46.66%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,28 +1,39 @@
-// m3hu1
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
-    ListNode* reverse(ListNode* head) {
-        ListNode *curr = head, *prev = NULL, *next = NULL;
-        while(curr) {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+public:
+    ListNode* reverse(ListNode* head){
+        if(head==nullptr||head->next==nullptr) return head;
+        ListNode* newHead = reverse(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = nullptr;
+        return newHead;
     }
-    public:
     bool isPalindrome(ListNode* head) {
-        ListNode *slow = head, *fast = head;
-        while(fast && fast->next) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next!=nullptr&&fast->next->next!=nullptr){
             slow = slow->next;
             fast = fast->next->next;
         }
-        slow = reverse(slow);
-        fast = head;
-        while(slow && fast) {
-            if(slow->val != fast->val) return false;
-            slow = slow->next;
-            fast = fast->next;
+        ListNode* newHead = reverse(slow->next);
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while(second!=nullptr){
+            if(first->val!=second->val){
+                return false;
+            }
+            first = first->next;
+            second = second->next;
         }
         return true;
     }
