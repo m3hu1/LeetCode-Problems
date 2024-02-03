c3de92ed8f1208888fe8861f3f7735ab99028489
Mehul Pathak <pathakkmehul@gmail.com>
2024-02-03 19:14:34 +0530

    Time: 20 ms (84.11%), Space: 94.5 MB (12.43%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,26 @@
-// m3hu1
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size() + nums2.size();
-        int merged[n];
-        int mid = n/2;
-        double median;
-        int i = 0, j = 0, k = 0;
-        int count = 0, curr;
-        while(count<n){
-            if(i<nums1.size() and j<nums2.size()){
-                if(nums1[i]<=nums2[j]){
-                    curr = nums1[i++];
-                }
-                else{
-                    curr = nums2[j++];
-                }
-            }
-            else if(i<nums1.size()){
-                curr = nums1[i++];
-            }
-            else{
-                curr = nums2[j++];
-            }
-            merged[k++] = curr;
-            count++;
+        int n = nums1.size();
+        int m = nums2.size();
+        if(n > m) return findMedianSortedArrays(nums2, nums1);
+        int req = (n + m + 1) / 2;
+        int low = 0, high = n;
+        while(low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = req - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            if(mid1 < n) r1 = nums1[mid1];
+            if(mid2 < m) r2 = nums2[mid2];
+            if(mid1 - 1 >=0) l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >=0) l2 = nums2[mid2 - 1];
+            if(l1 <= r2 && l2 <= r1) {
+                if((n+m)%2==1) return max(l1,l2);
+                else return (double)(max(l1,l2)+min(r1,r2))/2.0;
+            }else if(l1 > r2) high = mid1-1;
+            else low = mid1+1;
         }
-        if(n%2==0){
-            median = merged[mid] + merged[mid-1];
-            median /= 2;
-        }
-        else{
-            median = merged[mid];
-        }
-        return median;
+        return 0;
     }
 };
