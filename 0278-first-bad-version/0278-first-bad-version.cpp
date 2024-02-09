f8a592f2d24e9dd315100d9f27493accb250b1fd
Mehul Pathak <pathakkmehul@gmail.com>
2024-02-09 07:56:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int low = 0;
-        int high = n;
+        int high = n - 1;
         int ans = 0;
         while(low <= high) {
             int mid = low + (high - low) / 2;
