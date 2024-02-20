0957cb161d588f0830fbdc47d216bfabb9ebf92e
Mehul Pathak <pathakkmehul@gmail.com>
2024-02-20 20:58:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int sum = (n * (n+1))/2;
-        int sum2 = 0;
-        for(int i=0;i<n;i++) {
-            sum2 += nums[i];
-        }
-        return sum-sum2;
+        
     }
 };
