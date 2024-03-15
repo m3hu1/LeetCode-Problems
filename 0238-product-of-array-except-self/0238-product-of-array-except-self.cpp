1308e75cfa5d21e7afd0ac5ef70fbdf18d9b294e
Mehul Pathak <pathakkmehul@gmail.com>
2024-03-15 09:17:26 +0530

    Time: 19 ms (36.15%), Space: 26.4 MB (72.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        ios::sync_with_stdio(false); cin.tie(nullptr);
         vector<int> answer(nums.size(), 1);
         int prefix = 1;
         for (int i = 0; i < nums.size(); i++) {
