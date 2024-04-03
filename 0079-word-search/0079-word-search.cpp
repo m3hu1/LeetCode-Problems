b199bfc558eab982a77d9770a5ccf219fc8babe7
Mehul Pathak <pathakkmehul@gmail.com>
2024-04-03 13:33:26 +0530

    Time: 1016 ms (25.5%), Space: 11.9 MB (18.51%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,29 +1,27 @@
-// m3hu1
 class Solution {
- public:
-  bool exist(vector<vector<char>>& board, string word) {
-    for (int i = 0; i < board.size(); ++i)
-      for (int j = 0; j < board[0].size(); ++j)
-        if (dfs(board, word, i, j, 0))
-          return true;
-    return false;
-  }
- private:
-  bool dfs(vector<vector<char>>& board, const string& word, int i, int j,
-           int s) {
-    if (i < 0 || i == board.size() || j < 0 || j == board[0].size())
-      return false;
-    if (board[i][j] != word[s] || board[i][j] == '*')
-      return false;
-    if (s == word.length() - 1)
-      return true;
-    const char cache = board[i][j];
-    board[i][j] = '*';
-    const bool isExist = dfs(board, word, i + 1, j, s + 1) ||
-                         dfs(board, word, i - 1, j, s + 1) ||
-                         dfs(board, word, i, j + 1, s + 1) ||
-                         dfs(board, word, i, j - 1, s + 1);
-    board[i][j] = cache;
-    return isExist;
-  }
+public:
+    bool dfs(vector<vector<char>> &board, string word, int r, int c, int i) {
+        int rows = board.size();
+        int cols = board[0].size();
+        if (i == word.size()) return true;
+        if (r < 0 || c < 0 || r >= rows || c >= cols || word[i] != board[r][c]) {
+            return false;
+        }
+        char temp = board[r][c];
+        board[r][c] = '@';
+        if (dfs(board, word, r + 1, c, i + 1) || dfs(board, word, r - 1, c, i + 1) || dfs(board, word, r, c + 1, i + 1) || dfs(board, word, r, c - 1, i + 1)) return true;
+        board[r][c] = temp;
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int rows = board.size();
+        int cols = board[0].size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(board, word, i, j, 0) == true) return true;
+            }
+        }
+        return false;
+    }
 };
