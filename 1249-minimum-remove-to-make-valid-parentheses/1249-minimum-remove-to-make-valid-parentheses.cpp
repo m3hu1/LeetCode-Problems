5cbca6efe1b5e57d67ffb4406ff269c4e9b30645
Mehul Pathak <pathakkmehul@gmail.com>
2024-04-06 09:48:44 +0530

    Time: 26 ms (26.47%), Space: 12.9 MB (56.98%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack <int> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') st.push(i);
+            else if (s[i] == ')') {
+                if (!st.empty()) st.pop();
+                else s[i] = '#';
+            }
+        }
+        while (!st.empty()) {
+            s[st.top()] = '#';
+            st.pop();
+        }
+        string ans = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != '#') ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
