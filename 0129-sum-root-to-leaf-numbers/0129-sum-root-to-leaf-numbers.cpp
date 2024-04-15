87477db03ef9482ba86046c8bb26aa6652977a38
Mehul Pathak <pathakkmehul@gmail.com>
2024-04-15 07:54:52 +0530

    Time: 0 ms (100%), Space: 10.8 MB (96.99%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,37 +1,24 @@
-// m3hu1
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    void solve1(vector<int>v,int &sum){
-        int length=v.size();
-        int j=0;
-        for(int i=length-1;i>=0;i--){
-            sum+=v[j]*pow(10,i);
-            j++;
-        }
-        return;
-    }
-private:
-    void solve(TreeNode* root,vector<int>v,int &sum){
-        v.push_back(root->val);
-        if(root->left==NULL && root->right==NULL){
-            solve1(v,sum);
-            return;
-        }
-        if(root->left!=NULL){
-            solve(root->left,v,sum);
-        }
-        if(root->right!=NULL){
-            solve(root->right,v,sum);
-        }
-    }
 public:
+    int helper(TreeNode* node, int sum) {
+        if(!node) return 0;
+        sum = sum * 10 + node->val;
+        if(!node->left && !node->right) return sum;
+        return helper(node->left, sum) + helper(node->right, sum);
+    }
+
     int sumNumbers(TreeNode* root) {
-        int sum=0;
-        vector<int>v;
-        if(root==NULL){
-            return 0;
-        }
-        solve(root,v,sum);
-        return sum;
+        return helper(root, 0);
     }
 };
