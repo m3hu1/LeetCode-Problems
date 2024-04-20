270f60a8fda3552852dfc45e4db3886a29b43812
Mehul Pathak <pathakkmehul@gmail.com>
2024-04-20 13:37:33 +0530

    Time: 0 ms (100%), Space: 7.2 MB (28.86%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        while (numBottles >= numExchange) {
+            // int curr = numBottles / numExchange;
+            ans += numBottles / numExchange;
+            numBottles = numBottles % numExchange + numBottles / numExchange;
+        }
+        return ans;
+    }
+};
