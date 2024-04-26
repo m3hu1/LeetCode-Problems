6bcc8a3882cb2176dad81da712c97defdf21446e
Mehul Pathak <pathakkmehul@gmail.com>
2024-04-26 11:02:05 +0530

    Time: 17 ms (12.45%), Space: 13.5 MB (67.65%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
+    int minFallingPathSum(vector <vector <int>> &matrix) {
+        int ans = INT_MAX;
         int n = matrix.size();
-        for(int i = 1;i<n;i++){
-            for(int j = 0;j<n;j++){
-                 matrix[i][j] +=min({
-                    matrix[i-1][min(n-1,j+1)],
-                    matrix[i-1][j],
-                    matrix[i-1][max(0,j-1)]
-                });
+        vector <vector <int>> dp(n, vector <int>(n, INT_MAX));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) { // first row ke upar kuch nahi
+                    dp[i][j] = matrix[i][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                    if (j != 0) { // case 1
+                        dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
+                    }
+                    if (j != n - 1) { // case 2
+                        dp[i][j] = min(dp[i][j], dp[i - 1][j + 1]);
+                    }
+                    dp[i][j] += matrix[i][j];
+                }
             }
-        };
-       return *min_element( matrix.back().begin(), matrix.back().end());
+        }
+        // int minSum = INT_MAX;
+        // for (int j = 0; j < n; j++) {
+        //     minSum = min(minSum, dp[n - 1][j]);
+        // }
+        // return minSum;
+        return *min_element(dp[n - 1].begin(), dp[n - 1].end());
     }
 };
