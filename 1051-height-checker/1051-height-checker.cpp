b9efe8e5be94e87712a7194f02ae6d1bed755ba4
Mehul Pathak <pathakkmehul@gmail.com>
2024-06-10 10:13:53 +0530

    Time: 0 ms (100%), Space: 10.2 MB (10.73%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector <int> expected(heights.begin(), heights.end());
+        sort(expected.begin(), expected.end());
+        int cnt = 0;
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != expected[i]) cnt++;
+        }
+        return cnt;
+    }
+};
