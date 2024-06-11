b98fa970ce13178cf117cf597066c8025a493d01
Mehul Pathak <pathakkmehul@gmail.com>
2024-06-11 19:22:17 +0530

    Time: 43 ms (67.47%), Space: 16.8 MB (99.81%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif s[l].lower() != s[r].lower():
+                return False
+            else:
+                l += 1
+                r -= 1
+        
+        return True
