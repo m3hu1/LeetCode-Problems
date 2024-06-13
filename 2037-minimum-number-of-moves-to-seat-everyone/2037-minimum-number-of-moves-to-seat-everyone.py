3a0c5f905682050f6305821a5164c82b858d4524
Mehul Pathak <pathakkmehul@gmail.com>
2024-06-13 10:30:07 +0530

    Time: 58 ms (66.54%), Space: 16.4 MB (82.54%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
@@ -4,7 +4,10 @@ class Solution:
         stu.sort()
         ans = 0
 
-        for i in range(len(seats)):
-            ans += abs(stu[i] - seats[i])
+        # for i in range(len(seats)):
+        #     ans += abs(stu[i] - seats[i])
+
+        for seat, student in zip(seats, stu):
+            ans += abs(seat - student)
 
         return ans
