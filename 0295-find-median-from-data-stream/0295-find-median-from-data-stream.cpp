5202cac4b7e4a31868f98c20e36075545f11a3a1
Mehul Pathak <pathakkmehul@gmail.com>
2024-06-25 20:01:59 +0530

    Time: 249 ms (90.4%), Space: 121.8 MB (55.08%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,53 +1,36 @@
-// m3hu1
 class MedianFinder {
 public:
-    priority_queue<int, vector<int>, greater<int>> min_heap;
-    priority_queue<int, vector<int>> max_heap;
+    priority_queue <int> max_heap;
+    priority_queue <int, vector <int>, greater <int>> min_heap;
     MedianFinder() {
+        // if element bigger then push into max heap
+        // check later maybe wrong
     }
+    
     void addNum(int num) {
-        if (min_heap.size() == max_heap.size()) {
-            if (min_heap.size() == 0){
-                min_heap.push(num);
-                return;
-            }
-            if (num < min_heap.top()) {
-                max_heap.push(num);
-            } else {
-                min_heap.push(num);
-            }
-            return;
-        }
-        if (min_heap.size() < max_heap.size()) {
-            if (num > max_heap.top()) {
-                min_heap.push(num);
-            } else {
-                int top = max_heap.top();
-                min_heap.push(top);
-                max_heap.pop();
-                max_heap.push(num);
-            }
-            return;
-        }
-        if (min_heap.size() > max_heap.size()) {
-            if (num < min_heap.top()) {
-                max_heap.push(num);
-            } else {
-                int top = min_heap.top();
-                max_heap.push(top);
-                min_heap.pop();
-                min_heap.push(num);
-            }
-            return;
+        if (max_heap.empty() || num <= max_heap.top()) max_heap.push(num); // check if < or >
+        else min_heap.push(num);
+        if (max_heap.size() > min_heap.size() + 1) { // have to check this 
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+        } else if (min_heap.size() > max_heap.size()) {
+            max_heap.push(min_heap.top());
+            min_heap.pop();
         }
     }
+    // 2,3,4,5 --> 2, 3 4 5 -> 2, 3 and 4, 5 -> 2 and 3,4,5 even len
+    // odd len 1,2,3 1 and 2,3 -> ok got it
     double findMedian() {
-        if (min_heap.size() < max_heap.size()) {
-            return max_heap.top();
+        if (max_heap.size() == min_heap.size()) {
+            return (max_heap.top() + min_heap.top()) / 2.0;
         }
-        if (min_heap.size() > max_heap.size()) {
-            return min_heap.top();
-        }
-        return (max_heap.top() + min_heap.top())/2.0;
+        return max_heap.top();
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
