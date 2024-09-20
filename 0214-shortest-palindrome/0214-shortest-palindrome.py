2bf7ab0a5326c11a19083be90e0de8e914308523
Mehul Pathak <pathakkmehul@gmail.com>
2024-09-20 22:54:24 +0530

    Time: 72 ms (50.52%), Space: 17.1 MB (41.65%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        rev = s[ :: -1]
+        
+        for i in range(len(s)):
+            if s[ : len(s) - i] == rev[i : ]:
+                return rev[ : i] + s
+        
+        return ""
